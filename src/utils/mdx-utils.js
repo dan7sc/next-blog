caf28bdf5b8ea11130824cf53c46c60d88b1b5fb
dan7sc <dan7sc@users.noreply.github.com>
2024-09-04 16:21:55 -0300

    setup blog

---
+++ src/utils/mdx-utils.js
@@ -0,0 +1,21 @@
+export const getPosts = async () => {
+  const posts = [
+    {
+      id: 1,
+      title: "blog 1",
+      description: "description blog 1"
+    },
+    {
+      id: 2,
+      title: "blog 2",
+      description: "description blog 2"
+    },
+    {
+      id: 3,
+      title: "blog 3",
+      description: "description blog 3"
+    },
+  ]
+
+  return posts
+}
