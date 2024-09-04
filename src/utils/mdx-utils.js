14de020fce5a5780cda625dcb77546f3950ba92a
dan7sc <dan7sc@users.noreply.github.com>
2024-09-04 20:30:10 -0300

    add db.json and get posts

--- src/utils/mdx-utils.js
+++ src/utils/mdx-utils.js
@@ -1,21 +1,12 @@
-export const getPosts = async () => {
-  const posts = [
-    {
-      id: 1,
-      title: "blog 1",
-      description: "description blog 1"
-    },
-    {
-      id: 2,
-      title: "blog 2",
-      description: "description blog 2"
-    },
-    {
-      id: 3,
-      title: "blog 3",
-      description: "description blog 3"
-    },
-  ]
+import { api } from "../services/api"
 
-  return posts
+
+export const getPosts = async () => {
+  const { data } = await api.get('/posts');
+
+  if (data) {
+    return data;
+  }
+
+  return [];
 }
